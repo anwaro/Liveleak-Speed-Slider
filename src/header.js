5193f40532479e52377f8789ac13187302a624ed
Łukasz Micał <lukasz.mical@sarigato.com>
2017-10-24 19:51:30 +0200

    initial commit - old version

---
+++ src/header.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Liveleak Speed Slider
+// @version      0.1.3
+// @namespace    http://lukaszmical.pl/
+// @description  Liveleak Player Speed Slider
+// @author       Łukasz
+// @include      https://www.liveleak.com/ll_embed*
+// @include      https://www.liveleak.com/view*
+// @grant        none
+// ==/UserScript==
